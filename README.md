9d0c5d0623730b0bcd00756697687e0be30cf322
Vinicius Pereira <viniciusogp@gmail.com>
2020-07-15 14:15:59 -0300

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# liberteofuturo-site
---
+++ precache-manifest.2c850e25c6bf33c68baa95b967c85275.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cee9d930468048acd3f51555483a8dcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e665024fc4a4f0da3358",
+    "url": "/static/css/2.1d143842.chunk.css"
+  },
+  {
+    "revision": "4eb6feb3e69d2d698965",
+    "url": "/static/css/main.63398de4.chunk.css"
+  },
+  {
+    "revision": "e665024fc4a4f0da3358",
+    "url": "/static/js/2.1a3574d2.chunk.js"
+  },
+  {
+    "revision": "87e7d156e76bf0ee9b32832aeb012217",
+    "url": "/static/js/2.1a3574d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eb6feb3e69d2d698965",
+    "url": "/static/js/main.58fccf9c.chunk.js"
+  },
+  {
+    "revision": "df872cd9c40593d9f867",
+    "url": "/static/js/runtime-main.cdc07858.js"
+  },
+  {
+    "revision": "3f987b97d8b50adf51787fbfcc6b4146",
+    "url": "/static/media/HOME_DESTAQUE_13.3f987b97.jpg"
+  },
+  {
+    "revision": "6bf028b7f960d0d9776994e86f36fd36",
+    "url": "/static/media/HOME_DESTAQUE_2.6bf028b7.jpg"
+  },
+  {
+    "revision": "7b79e5add549de126120e41ccc311208",
+    "url": "/static/media/HOME_DESTAQUE_4.7b79e5ad.jpg"
+  },
+  {
+    "revision": "e57b038e388eb031f8b083cad88d451d",
+    "url": "/static/media/antidotos.e57b038e.svg"
+  },
+  {
+    "revision": "fe28fe8c306283741caee09281ffac2c",
+    "url": "/static/media/antidotos.fe28fe8c.jpg"
+  },
+  {
+    "revision": "e95fab9f6bf8b881942506dbd8bf7c07",
+    "url": "/static/media/back.e95fab9f.mp4"
+  },
+  {
+    "revision": "75e4d7f000f816b591416cf5d8599bf1",
+    "url": "/static/media/capa1.75e4d7f0.jpg"
+  },
+  {
+    "revision": "13e936cffd41dc2db8dbf836c2b09962",
+    "url": "/static/media/clima.13e936cf.svg"
+  },
+  {
+    "revision": "75e4d7f000f816b591416cf5d8599bf1",
+    "url": "/static/media/clima.75e4d7f0.jpg"
+  },
+  {
+    "revision": "3e942ff940f7cfa975a16a3c1b567cff",
+    "url": "/static/media/consumo.3e942ff9.svg"
+  },
+  {
+    "revision": "692bb811c2182e87274838eda1a2a0b8",
+    "url": "/static/media/consumo.692bb811.jpg"
+  },
+  {
+    "revision": "4feb74da637b40bca62118215c7a29c2",
+    "url": "/static/media/democracia.4feb74da.jpg"
+  },
+  {
+    "revision": "9a07f030983a273f42a36700edb27331",
+    "url": "/static/media/democracia.9a07f030.svg"
+  },
+  {
+    "revision": "7a19298b2646472f80734e54228f0ea9",
+    "url": "/static/media/fundo.7a19298b.svg"
+  },
+  {
+    "revision": "530f0502803d246c76528376bc9b4f5e",
+    "url": "/static/media/insurreicao.530f0502.svg"
+  },
+  {
+    "revision": "dee0408d2acff279b88dcfd7e436a0f4",
+    "url": "/static/media/insurreicao.dee0408d.jpg"
+  },
+  {
+    "revision": "2b1801b473cad17d391ada208934d144",
+    "url": "/static/media/laboratorio.2b1801b4.jpg"
+  },
+  {
+    "revision": "45fa73902b479e7a037549109257e6b4",
+    "url": "/static/media/logo_claro.45fa7390.svg"
+  },
+  {
+    "revision": "30f8fd2efe531a1b587337c835537f17",
+    "url": "/static/media/logo_horizontal.30f8fd2e.svg"
+  },
+  {
+    "revision": "bd4d1e00fa80b96a2e29408d0590ddb3",
+    "url": "/static/media/logo_vertical.bd4d1e00.svg"
+  },
+  {
+    "revision": "2ca0bd1a59d6071e6a88d8eb6be90466",
+    "url": "/static/media/movimento.2ca0bd1a.jpg"
+  },
+  {
+    "revision": "0dbad316a5ff2ed12b5ff40713616a0a",
+    "url": "/static/media/participe.0dbad316.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "8b4764350833680aab1f241724d1478f",
+    "url": "/static/media/teaser.8b476435.mp4"
+  }
+]);
